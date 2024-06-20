c4d784eb336b7d28eb27a8bd930e13fa591badbd
NadezhdaT <kybalibri@mail.ru>
2024-06-20 19:56:14 +0700

    initial commit

---
+++ src/test/java/ru/netology/rest.java
@@ -0,0 +1,26 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class rest {
+    @Test
+    void shouldReturnSendData() {
+        String text = "Hi";
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+        ;
+    }
+}
